d9e18b3ab470011ae6cec340bd82bc379f43e78f
Gene <genewang7@gmail.com>
2023-07-25 14:34:10 +0800

    feat: init

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,46 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container mt-4">
+    <h1>Transaction Monitor</h1>
+    <div class="input-group mb-3">
+      <div class="col-sm-1 col-form-label">
+        <label class="my-auto">User Iban:</label>
+      </div>
+      <div class="col my-auto">
+        <select class="form-select" aria-label="user">
+          <option selected>Select user</option>
+          <option value="1">One</option>
+          <option value="2">Two</option>
+          <option value="3">Three</option>
+        </select>
+      </div>
+    </div>
+    <button class="btn btn-success m-3">Insert 10 records to the user</button>
+    <button class="btn btn-primary m-3">Get all records of the user</button>
+    <!-- <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Investment</h5>
+        <h6 class="card-subtitle mb-2 text-muted">ARS 78</h6>
+        <p class="card-text">2022-07-12</p>
+      </div>
+    </div> -->
+    <div class="row row-cols-1 row-cols-md-3">
+      <div class="col mb-4" v-for="card in cards" :key="card.id">
+        <div class="card">
+          <img :src="card.imageUrl" class="card-img-top" alt="Card Image">
+          <div class="card-body">
+            <h5 class="card-title">{{ card.title }}</h5>
+            <h5 class="card-subtitle">{{ card.subtitle }}</h5>
+            <p class="card-text">{{ card.description }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+
 </script>
 
 <style>
