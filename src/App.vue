6d0c9cccd0e357116b494f4dea99ce33766825a0
Gene <genewang7@gmail.com>
2023-07-25 20:37:17 +0800

    feat: UI done

--- src/App.vue
+++ src/App.vue
@@ -16,31 +16,84 @@
     </div>
     <button class="btn btn-success m-3">Insert 10 records to the user</button>
     <button class="btn btn-primary m-3">Get all records of the user</button>
-    <!-- <div class="card">
-      <div class="card-body">
-        <h5 class="card-title">Investment</h5>
-        <h6 class="card-subtitle mb-2 text-muted">ARS 78</h6>
-        <p class="card-text">2022-07-12</p>
-      </div>
-    </div> -->
+
+    <!-- Cards -->
     <div class="row row-cols-1 row-cols-md-3">
-      <div class="col mb-4" v-for="card in cards" :key="card.id">
+      <div class="col mb-4" v-for="card in paginatedCards" :key="card.id">
         <div class="card">
-          <img :src="card.imageUrl" class="card-img-top" alt="Card Image">
           <div class="card-body">
-            <h5 class="card-title">{{ card.title }}</h5>
-            <h5 class="card-subtitle">{{ card.subtitle }}</h5>
-            <p class="card-text">{{ card.description }}</p>
+            <h5 class="card-title">{{ card.description }}</h5>
+            <h5 class="card-subtitle">{{ card.amountWithCurrency }}</h5>
+            <p class="card-text">{{ card.date }}</p>
           </div>
         </div>
       </div>
     </div>
+
+    <!-- Pagination -->
+    <nav aria-label="Page navigation">
+      <ul class="pagination justify-content-center">
+        <li class="page-item" :class="{ disabled: currentPage === 1 }">
+          <button class="page-link" @click="prevPage" :disabled="currentPage === 1">Previous</button>
+        </li>
+        <li class="page-item" v-for="page in totalPages" :key="page" :class="{ active: currentPage === page }">
+          <button class="page-link" @click="gotoPage(page)">{{ page }}</button>
+        </li>
+        <li class="page-item" :class="{ disabled: currentPage === totalPages }">
+          <button class="page-link" @click="nextPage" :disabled="currentPage === totalPages">Next</button>
+        </li>
+      </ul>
+    </nav>
   </div>
 </template>
 
 <script setup>
+import { computed, ref } from 'vue';
+
+const cards = ref([])
+const CARDS_PER_PAGE = 12
+const currentPage = ref(1)
+
+const initCards = () => {
+  let cardObj = {
+    id: 'fbfe820f-f017-4428-93cb-a18af834f4c6',
+    iban: 'MD130V0JGFQUND01H9MJJB0C',
+    description: 'Investment',
+    amountWithCurrency: 'ARS 78',
+    date: '2022-07-12'
+  }
+  for (let i = 0; i < 20; i++)
+    cards.value.push(cardObj)
+}
+
+const paginatedCards = computed(() => {
+  const startIndex = (currentPage.value - 1) * CARDS_PER_PAGE;
+  const endIndex = startIndex + CARDS_PER_PAGE;
+  return cards.value.slice(startIndex, endIndex);
+})
+
+const totalPages = computed(() => {
+  return Math.ceil(cards.value.length / CARDS_PER_PAGE);
+})
+
+const gotoPage = (pageNumber) => {
+  currentPage.value = pageNumber
+}
+
+const prevPage = () => {
+  if (currentPage.value > 1) {
+    currentPage.value--;
+  }
+}
+
+const nextPage = () => {
+  if (currentPage.value < totalPages.value) {
+    currentPage.value++;
+  }
+}
 
 
+initCards()
 </script>
 
 <style>
